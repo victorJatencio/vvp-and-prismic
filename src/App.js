b54c5e683f6b68b779c6cc73356953dd586061da
victorJatencio <victoj.atencio@gmail.com>
2019-12-02 13:24:17 -0500

    Create a first page sample successfully

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React, { Fragment } from 'react'
-import { Helmet } from 'react-helmet'
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom'
-import { apiEndpoint } from './prismic-configuration'
-import { Help, Preview, NotFound } from './pages'
+import React, { Fragment } from "react";
+import { Helmet } from "react-helmet";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { apiEndpoint } from "./prismic-configuration";
+import { Help, Preview, NotFound } from "./pages";
+import Page from "./pages/Page";
 
 /**
  * Main application componenet
  */
-const App = (props) => {
-  const repoNameArray = /([^/]+)\.cdn.prismic\.io\/api/.exec(apiEndpoint)
-  const repoName = repoNameArray[1]
+const App = props => {
+  const repoNameArray = /([^/]+)\.cdn.prismic\.io\/api/.exec(apiEndpoint);
+  const repoName = repoNameArray[1];
 
   return (
     <Fragment>
       <Helmet>
-        <script async defer src={`//static.cdn.prismic.io/prismic.js?repo=${repoName}&new=true`} />
+        <script
+          async
+          defer
+          src={`//static.cdn.prismic.io/prismic.js?repo=${repoName}&new=true`}
+        />
       </Helmet>
       <BrowserRouter>
         <Switch>
-          <Redirect exact from='/' to='/help' />
-          <Route exact path='/help' component={Help} />
-          <Route exact path='/preview' component={Preview} />
+          <Redirect exact from="/" to="/help" />
+          <Route exact path="/help" component={Help} />
+          <Route exact path="/preview" component={Preview} />
+          <Route exact path="/page/:uid" component={Page} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
     </Fragment>
-  )
-}
+  );
+};
 
-export default App
+export default App;
